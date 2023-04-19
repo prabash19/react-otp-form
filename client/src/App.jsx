425eae209bbac6d0d9310a1f38aa78d5894c6745
suprabash <suprabash@esignature.com.np>
2023-04-19 13:34:29 +0545

    folder structure changed for server

---
+++ client/src/App.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "./App.css";
+function App() {
+  const [otp, setOtp] = React.useState(new Array(6).fill(""));
+  const [errorMesage, setErrorMessage] = useState(false);
+  //input value get
+  const handleChange = (el, index) => {
+    if (isNaN(el.value)) {
+      // if input is not a number, set value to empty string and add highlight class
+      // el.value = "";
+      setOtp([...otp.map((data, i) => (i === index ? el.value : data))]);
+      el.classList.add("highlight");
+    } else {
+      // if input is a number, update state with input value and remove highlight class
+      setOtp([...otp.map((data, i) => (i === index ? el.value : data))]);
+      el.classList.remove("highlight");
+    }
+
+    if (el.nextSibling) {
+      el.nextSibling.focus();
+    }
+  };
+  //onClick event
+  const submintOtp = () => {
+    otp.join("") === "111111" ? alert("verified") : alert("wrong otp");
+    const otpValue = Number(otp.join(""));
+    console.log("otp value is", otpValue);
+  };
+  const handlePaste = (e) => {
+    e.preventDefault();
+    const pastedData = e.clipboardData.getData("text/plain");
+    const otpArray = pastedData
+      .split("")
+      .filter((data) => !isNaN(data))
+      .slice(0, 6);
+    const newOtp = [...otp];
+    otpArray.forEach((data, index) => {
+      newOtp[index] = data;
+    });
+    setOtp(newOtp);
+  };
+  return (
+    <div className="container">
+      <h3>Please Enter Your OTP</h3>
+      {errorMesage && (
+        <p>
+          The OTP was not correct.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Please enter again
+        </p>
+      )}
+
+      <div className="otp-input-box">
+        {otp.map((data, i) => {
+          return (
+            <input
+              type="text"
+              name="otp"
+              className={`otp-input ${isNaN(data) ? "highlight" : ""}`}
+              maxLength={1}
+              key={i}
+              value={data}
+              onChange={(e) => handleChange(e.target, i)}
+              onFocus={(e) => e.target.select()}
+              onPaste={handlePaste}
+              onBlur={(e) => e.target.classList.remove("highlight")}
+            />
+          );
+        })}
+      </div>
+      <button onClick={submintOtp} className="button">
+        <p>Submit</p>
+      </button>
+    </div>
+  );
+}
+
+export default App;
